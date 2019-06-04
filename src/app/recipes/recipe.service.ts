435aaaa6b9e742ba9c09e37185275168084f8ad5
m3c0d3 <m3c0d3@gmail.com>
2019-06-04 22:23:23 +0300

    121. Passing Ingredients from Recipes to the Shopping List (via a Service)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,18 +1,38 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/Ingredient';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe',
-      'This is simply a test',
-      'http://www.rajnisrecipe.com/wp-content/uploads/2016/12/pasta.jpg'),
-    new Recipe('Another Test Recipe',
-      'This is simply a test2',
-      'http://www.manjulaskitchen.com/blog/wp-content/uploads/bread_burfi.jpg?x45960')
+    new Recipe('Tasty Schnitzel',
+      'A super tasty Schnitzel - just awesome',
+      'http://www.rajnisrecipe.com/wp-content/uploads/2016/12/pasta.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]
+    ),
+    new Recipe('Big Fat Burger',
+      'What else can you say?',
+      'http://www.manjulaskitchen.com/blog/wp-content/uploads/bread_burfi.jpg?x45960',
+      [
+        new Ingredient('Buns', 1),
+        new Ingredient('Meat', 2)
+      ]
+    )
   ];
+
+  constructor(private slService: ShoppingListService) {
+  }
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
